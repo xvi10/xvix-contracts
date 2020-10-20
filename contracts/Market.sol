aa5c687cd10c918ba7188656700f25e2d93586c7
Phi <phi@lattefi.app>
2020-10-20 17:24:21 +0800

    improve market swaps

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -4,47 +4,116 @@ pragma solidity =0.6.12;
 
 import "./libraries/math/SafeMath.sol";
 import "./libraries/token/IERC20.sol";
+import "./uniswap/UniswapV2Library.sol";
+import "./uniswap/TransferHelper.sol";
 
 import "./interfaces/IPool.sol";
-import "./interfaces/IUniswapV2Router.sol";
+import "./interfaces/ILatte.sol";
+import "./interfaces/IWETH.sol";
+import "./interfaces/IUniswapV2Factory.sol";
 
 contract Market {
     using SafeMath for uint256;
 
-    address public immutable latte;
-    address public immutable pool;
-    address public immutable uniswapRouter;
+    uint256 public constant BASIS_POINTS_DIVISOR = 10000;
 
-    constructor(address _latte, address _pool, address _uniswapRouter) public {
+    address public immutable latte;
+    address public immutable WETH;
+    address public immutable pool;
+    address public immutable factory;
+
+    modifier ensure(uint deadline) {
+        require(deadline >= block.timestamp, "Market: expired");
+        _;
+    }
+
+    constructor(address _latte, address _weth, address _pool, address _factory) public {
         latte = _latte;
+        WETH = _weth;
         pool = _pool;
-        uniswapRouter = _uniswapRouter;
+        factory = _factory;
     }
 
     function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
         external
         payable
+        ensure(deadline)
         returns (uint[] memory amounts)
     {
         require(path[path.length - 1] == latte, "Market: path does not end with latte");
-        amounts = IUniswapV2Router(uniswapRouter).swapExactETHForTokens{value: msg.value}(amountOutMin, path, to, deadline);
-        uint256 amount = amounts[amounts.length - 1];
-        IPool(pool).mint(msg.sender, amount);
+
+        require(path[0] == WETH, 'UniswapV2Router: INVALID_PATH');
+        amounts = UniswapV2Library.getAmountsOut(factory, msg.value, path);
+        require(amounts[amounts.length - 1] >= amountOutMin, 'UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT');
+        IWETH(WETH).deposit{value: amounts[0]}();
+        assert(IWETH(WETH).transfer(UniswapV2Library.pairFor(factory, path[0], path[1]), amounts[0]));
+        _swap(amounts, path, to);
+
+        IPool(pool).mint(msg.sender, amounts[amounts.length - 1]);
     }
 
     function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
         external
         payable
+        ensure(deadline)
         returns (uint[] memory amounts)
     {
         require(path[path.length - 1] == latte, "Market: path does not end with latte");
-        amounts = IUniswapV2Router(uniswapRouter).swapETHForExactTokens{value: msg.value}(amountOut, path, to, deadline);
-        uint256 amount = amounts[amounts.length - 1];
-        IPool(pool).mint(msg.sender, amount);
 
-        if (msg.value > amounts[0]) {
-            (bool success,) = msg.sender.call{value: msg.value.sub(amounts[0])}("");
-            require(success, "Market: transfer failed");
+        require(path[0] == WETH, 'UniswapV2Router: INVALID_PATH');
+        amounts = UniswapV2Library.getAmountsIn(factory, amountOut, path);
+        require(amounts[0] <= msg.value, 'UniswapV2Router: EXCESSIVE_INPUT_AMOUNT');
+        IWETH(WETH).deposit{value: amounts[0]}();
+        assert(IWETH(WETH).transfer(UniswapV2Library.pairFor(factory, path[0], path[1]), amounts[0]));
+        _swap(amounts, path, to);
+        // refund dust eth, if any
+        if (msg.value > amounts[0]) TransferHelper.safeTransferETH(msg.sender, msg.value - amounts[0]);
+
+        IPool(pool).mint(msg.sender, amounts[amounts.length - 1]);
+    }
+
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        ensure(deadline)
+        returns (uint[] memory amounts)
+    {
+        require(path[path.length - 1] == WETH, 'UniswapV2Router: INVALID_PATH');
+        amounts = UniswapV2Library.getAmountsOut(factory, amountIn, path);
+        require(amounts[amounts.length - 1] >= amountOutMin, 'UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT');
+        TransferHelper.safeTransferFrom(
+            path[0], msg.sender, UniswapV2Library.pairFor(factory, path[0], path[1]), amounts[0]
+        );
+        _swap(amounts, path, address(this));
+        IWETH(WETH).withdraw(amounts[amounts.length - 1]);
+        TransferHelper.safeTransferETH(to, amounts[amounts.length - 1]);
+    }
+
+    function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+        external
+        ensure(deadline)
+        returns (uint[] memory amounts)
+    {
+        require(path[path.length - 1] == WETH, 'UniswapV2Router: INVALID_PATH');
+        amounts = UniswapV2Library.getAmountsIn(factory, amountOut, path);
+        require(amounts[0] <= amountInMax, 'UniswapV2Router: EXCESSIVE_INPUT_AMOUNT');
+        TransferHelper.safeTransferFrom(
+            path[0], msg.sender, UniswapV2Library.pairFor(factory, path[0], path[1]), amounts[0]
+        );
+        _swap(amounts, path, address(this));
+        IWETH(WETH).withdraw(amounts[amounts.length - 1]);
+        TransferHelper.safeTransferETH(to, amounts[amounts.length - 1]);
+    }
+
+    function _swap(uint[] memory amounts, address[] memory path, address _to) private {
+        for (uint i; i < path.length - 1; i++) {
+            (address input, address output) = (path[i], path[i + 1]);
+            (address token0,) = UniswapV2Library.sortTokens(input, output);
+            uint amountOut = amounts[i + 1];
+            (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOut) : (amountOut, uint(0));
+            address to = i < path.length - 2 ? UniswapV2Library.pairFor(factory, output, path[i + 2]) : _to;
+            IUniswapV2Pair(UniswapV2Library.pairFor(factory, input, output)).swap(
+                amount0Out, amount1Out, to, new bytes(0)
+            );
         }
     }
 
