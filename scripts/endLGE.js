e319523ffe26e8d47cf38b2128f7d873e5a3e7f5
Phi <phi@lattefi.app>
2020-11-23 16:53:47 +0800

    update scripts

--- scripts/endLGE.js
+++ scripts/endLGE.js
@@ -1,9 +1,14 @@
 const { sendTxn, contractAt } = require("./helpers")
 
 async function main() {
+  const provider = waffle.provider
+  const accounts = await provider.listAccounts()
+  const fund = { address: accounts[0] }
+  const xvix = await contractAt("XVIX", "0x61f1D54EeF55A54D4826B79E33fCf370488813e4")
   const distributor = await contractAt("Distributor", "0x5bf22F4473b0e2f96eb567a1BDf828012db2e2CB")
   const deadline = parseInt(Date.now() / 1000) + 20 * 60
   await sendTxn(distributor.endLGE(deadline), "distributor.endLGE")
+  await sendTxn(xvix.setFund(fund.address), "xvix.setFund")
 }
 
 main()
