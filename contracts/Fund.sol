656a75e57c0a7b25b3d95058274d42192bfe2a02
Phi <phi@lattefi.app>
2020-12-09 19:31:32 +0800

    update fund

--- contracts/Fund.sol
+++ contracts/Fund.sol
@@ -9,14 +9,17 @@ contract Fund {
     using SafeMath for uint256;
 
     uint256 public constant BASIS_POINTS_DIVISOR = 10000;
-    uint256 public constant FEE_SPLIT = 9000;
+    uint256 public constant FEE_SPLIT_A = 6500; // 65%
+    uint256 public constant FEE_SPLIT_B = 2700; // 27%
 
     address public receiverA;
     address public receiverB;
+    address public receiverC;
 
-    constructor(address _receiverA, address _receiverB) public {
+    constructor(address _receiverA, address _receiverB, address _receiverC) public {
         receiverA = _receiverA;
         receiverB = _receiverB;
+        receiverC = _receiverC;
     }
 
     function setReceiverA(address _receiverA) public {
@@ -29,11 +32,18 @@ contract Fund {
         receiverB = _receiverB;
     }
 
+    function setReceiverC(address _receiverC) public {
+        require(msg.sender == receiverC, "Fund: forbidden");
+        receiverC = _receiverC;
+    }
+
     function withdraw(address _token, uint256 _amount) public {
-        require(msg.sender == receiverA || msg.sender == receiverB, "Fund: forbidden");
-        uint256 amountA = _amount.mul(FEE_SPLIT).div(BASIS_POINTS_DIVISOR);
-        uint256 amountB = _amount.sub(amountA);
+        require(msg.sender == receiverA || msg.sender == receiverB || msg.sender == receiverC, "Fund: forbidden");
+        uint256 amountA = _amount.mul(FEE_SPLIT_A).div(BASIS_POINTS_DIVISOR);
+        uint256 amountB = _amount.mul(FEE_SPLIT_B).div(BASIS_POINTS_DIVISOR);
+        uint256 amountC = _amount.sub(amountA).sub(amountB);
         IERC20(_token).transfer(receiverA, amountA);
         IERC20(_token).transfer(receiverB, amountB);
+        IERC20(_token).transfer(receiverC, amountC);
     }
 }
